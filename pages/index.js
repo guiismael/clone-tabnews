5a12f2065ee610dbf7164572fe39ed55be4c23c7
Guilherme Ismael <guiismael@gmail.com>
2025-11-27 17:50:39 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Somos o que fazemos repetidamente. A excelência não é um feito, mas sim um hábito.</h1>
+  return (
+    <h1>
+      Somos o que fazemos repetidamente. A excelência não é um feito, mas sim um
+      hábito.
+    </h1>
+  );
 }
 
 export default Home;
