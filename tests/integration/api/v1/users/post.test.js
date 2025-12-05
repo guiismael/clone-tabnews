354948b5e65c06602b83ce59bbab8815c58ac348
Guilherme Ismael <guiismael@gmail.com>
2025-12-05 18:29:51 -0300

    feat: add `features` column to `users` table

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -32,6 +32,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "ValidData",
         email: "valid.data@email.com",
+        features: [],
         password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
