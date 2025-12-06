7db068e7d8e980a0db185eeea92fe7a58d4bbcca
Guilherme Ismael <guiismael@gmail.com>
2025-12-06 02:15:49 -0300

    feat: add `injectAnonymousOrUser` and `canRequest` middlewares to `/api/v1/sessions`

--- infra/controller.js
+++ infra/controller.js
@@ -1,6 +1,7 @@
 import * as cookie from "cookie";
 
 import {
+  ForbidderError,
   InternalServerError,
   MethodNotAllowedError,
   NotFoundError,
@@ -8,6 +9,7 @@ import {
   ValidationError,
 } from "infra/errors.js";
 import session from "models/session.js";
+import user from "models/user.js";
 
 function onNoMatchHandler(request, response) {
   const publicErrorObject = new MethodNotAllowedError();
@@ -15,7 +17,11 @@ function onNoMatchHandler(request, response) {
 }
 
 function onErrorHandler(error, request, response) {
-  if (error instanceof NotFoundError || error instanceof ValidationError) {
+  if (
+    error instanceof ForbidderError ||
+    error instanceof NotFoundError ||
+    error instanceof ValidationError
+  ) {
     return response.status(error.statusCode).json(error);
   }
 
@@ -55,6 +61,53 @@ function clearSessionCookie(response) {
   response.setHeader("Set-Cookie", setCookie);
 }
 
+async function injectAnonymousOrUser(request, response, next) {
+  if (request.cookies?.session_id) {
+    await injectAuthenticatedUser(request);
+    return next();
+  }
+
+  injectAnonymousUser(request);
+  return next();
+}
+
+async function injectAuthenticatedUser(request) {
+  const sessionToken = request.cookies.session_id;
+  const sessionObject = await session.findOneValidByToken(sessionToken);
+  const userObject = await user.findOneById(sessionObject.user_id);
+
+  request.context = {
+    ...request.context,
+    user: userObject,
+  };
+}
+
+function injectAnonymousUser(request) {
+  const anonymousUserObject = {
+    features: ["read:activation_tokens", "create:session", "create:user"],
+  };
+
+  request.context = {
+    ...request.context,
+    user: anonymousUserObject,
+  };
+}
+
+function canRequest(feature) {
+  return function canRequestMiddleware(request, response, next) {
+    const userTryingToRequest = request.context.user;
+
+    if (userTryingToRequest.features.includes(feature)) {
+      return next();
+    }
+
+    throw new ForbidderError({
+      message: "Você não possui permissão para execultar esta ação.",
+      action: `Verifique se o seu usuário possui a feature "${feature}".`,
+    });
+  };
+}
+
 const controller = {
   errorHandlers: {
     onNoMatch: onNoMatchHandler,
@@ -62,6 +115,8 @@ const controller = {
   },
   setSessionCookie,
   clearSessionCookie,
+  injectAnonymousOrUser,
+  canRequest,
 };
 
 export default controller;
