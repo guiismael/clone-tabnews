7b9b13ee759a84a075985fab153588e3d4429ca5
Guilherme Ismael <guiismael@gmail.com>
2025-12-19 16:23:35 -0300

    fix: change email auth params

--- infra/email.js
+++ infra/email.js
@@ -4,8 +4,8 @@ const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
   port: process.env.EMAIL_SMTP_PORT,
   auth: {
-    host: process.env.EMAIL_SMTP_USER,
-    port: process.env.EMAIL_SMTP_PASSWORD,
+    user: process.env.EMAIL_SMTP_USER,
+    pass: process.env.EMAIL_SMTP_PASSWORD,
   },
   secure: process.env.NODE_ENV === "production" ? true : false,
 });
